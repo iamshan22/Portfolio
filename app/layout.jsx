aaf5bec83185b03e86f9492a92c80b1e55ab0c06
iamshan22 <konashankaraditya@gmail.com>
2025-02-18 16:22:39 +0530

    Initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Weight } from "lucide-react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const jetbrainsMono = JetBrains_Mono({
+  variable:'--font-jetbrainsMono',
   subsets: ["latin"],
+  Weight: ["100","200","300","400","500","600","700","800"],
+  
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={jetbrainsMono.variable}
       >
         {children}
       </body>
---
+++ tailwind.config.js
@@ -0,0 +1,65 @@
+const { DEFAULT_CIPHERS } = require('tls');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  prefix:"",
+  theme: {
+	container:{
+		center:true,
+		padding: "15px",
+	},
+	screen:{
+		sm: "640px",
+		md: "768px",
+		lg: "960px",
+		xl: "1200px",
+
+	},
+	fontFamily:{
+		primary: "var(--font-jetbrainsMono)",
+
+	},
+
+
+  	extend: {
+
+  		colors: {
+			primary:'#1c1c22',
+			accent:{
+				DEFAULT:'#00ff99',
+				hover:'#00e187',
+			}
+  			
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+		keyframes:{
+			"accordtion-down":{
+				from:{height:"0"},
+				to:{height:'var(--radix-accordtion-content-height)'},
+
+			},
+			"accordtion-up":{
+				from:{height:'var(--radix-accordtion-content-height)'},
+				to:{height:"0"},
+
+			},
+		},
+		animation:{
+			"accordtion-down": "accordtion-down 0.2s ease-out",
+			"accordtion-up": "accordtion-up 0.2s ease-out",
+		},
+  	},
+
+  },
+  plugins: [require("tailwindcss-animate")],
+};

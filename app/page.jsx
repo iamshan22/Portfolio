09f4f43b5487552611265f0dcc613e524ca578ff
Kona Shankar Aditya <konashankaraditya@gmail.com>
2025-03-11 20:27:49 +0530

    v1

--- app/page.jsx
+++ app/page.jsx
@@ -18,7 +18,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none ">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Kona Shankar</span>
+              Hello I'm <br /> <span className="text-accent">Shan Kona</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">I excel at crafting elegent digital experiences and I am proficient in
               in various programming languges and technologies
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -57,19 +57,17 @@ const Services = () => {
                                 {/*TOP */}
                                 <div className="w-full flex justify-between items-center">
                                     <div className="text-5xl font-extrabold  
-                                    text-outline text-transparent group-hover:text-outline-hover"
-                                        transition-all duration-00>
+                                     text-outline text-transparent group-hover:text-outline-hover transition-all duration-00">
                                         {service.num}</div>
                                     <Link href={service.href} className="w-[70px] h-[70px] rounded-full
-                                    bg-white group-hover:bg-accent transition-all duration-500
+                                     bg-white group-hover:bg-accent transition-all duration-500
                                       flex justify-center items-center hover:-rotate-45" >
                                         <BsArrowDownRight className="text-primary text-3xl " />
                                     </Link>
                                 </div>
 
                                 {/*title*/}
-                                <h2 className="text-[42px] font-bold text-white group-hover:text-accent 
-                                 transition-all duration-500">
+                                <h2 className="text-[42px] font-bold text-white group-hover:text-accent  transition-all duration-500">
                                     {service.title}</h2>
                                 {/*Description*/}
                                 <p>{service.description}</p>
